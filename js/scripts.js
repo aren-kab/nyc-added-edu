7900d4c7f0034c1a3c235138a17aff0d38cfd65a
aren-kab <akabarajian@gmail.com>
2021-03-13 18:00:59 -0500

    commit_initial_map

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJlbi1rYWIiLCJhIjoiY2tsMTJhejk3MHhxazJxcW5sbGo0d3R3bSJ9.iuLNkGjRTZohqeqRzq-r7g';
+
+var map = new mapboxgl.Map({
+  container: 'mapContainer', // container ID
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  center: [-73.984771, 40.727157], // starting position [lng, lat]
+  zoom: 11 // starting zoom
+});
