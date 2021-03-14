2717e98025c2a894f47b1c0bf6c711aecfbbec11
aren-kab <akabarajian@gmail.com>
2021-03-14 00:30:48 -0500

    commit_colleges

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,29 @@ var geocoder = new MapboxGeocoder({ // Initialize the geocoder
 map.addControl(geocoder);
 //add nav control
 map.addControl(new mapboxgl.NavigationControl())
+
+map.on('load', function () {
+// Add an image to use as a custom marker
+  map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+  function (error, image) {
+    if (error) throw error;
+  map.addImage('custom-marker', image);
+  });
+});
+
+//let's add colleges to the map first
+map.on('style.load', function () {
+  map.addSource('colleges', {
+   type: 'geojson',
+   data: 'data/colleges.geojson'
+  });
+
+  map.addLayer({
+    'id': 'labels',
+    'type': 'symbol',
+    'source': 'colleges',
+    'layout': {
+    'icon-image': 'custom-marker'
+    }
+  });
+});
