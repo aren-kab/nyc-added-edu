08960c058757206242a84371a1e533e982232d05
aren-kab <akabarajian@gmail.com>
2021-03-20 18:29:50 -0400

    push directions

--- js/scripts.js
+++ js/scripts.js
@@ -225,29 +225,3 @@ document.getElementById("Bachelors").addEventListener('change', function(e) {
        });
      }
    })
-
-// // map.on('load', function () {
-// // Add an image to use as a custom marker
-//   map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-//   function (error, image) {
-//     if (error) throw error;
-//   map.addImage('custom-marker', image);
-//   });
-// });
-
-// //let's add colleges to the map first
-// map.on('style.load', function () {
-//   map.addSource('colleges', {
-//    type: 'geojson',
-//    data: 'data/colleges.geojson'
-//   });
-//
-//   map.addLayer({
-//     'id': 'labels',
-//     'type': 'symbol',
-//     'source': 'colleges',
-//     'layout': {
-//     'icon-image': 'custom-marker'
-//     }
-//   });
-// });
