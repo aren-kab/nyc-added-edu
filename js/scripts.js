57ab0c828e71051c2b27bfe8a2155f6711d8031e
aren-kab <akabarajian@gmail.com>
2021-03-24 15:00:39 -0400

    testerr

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ var map = new mapboxgl.Map({
 });
 
 // disable map zoom when using scroll
-// map.scrollZoom.disable();
+map.scrollZoom.disable();
 
 var geocoder = new MapboxGeocoder({ // Initialize the geocoder
   accessToken: mapboxgl.accessToken, // Set the access token
@@ -248,3 +248,28 @@ document.getElementById("Bachelors").addEventListener('change', function(e) {
       $(this).show();
     });
   })
+
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("about");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
